5c7daf8ae9dac3363387084ce23e140f1c708896
admin <admin@USER>
2023-07-23 22:35:15 +0600

    Add project files.

---
+++ api2/Controllers/CategoryController.cs
@@ -0,0 +1,78 @@
+﻿using api2.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api2.Controllers
+{
+    public class CategoryController : ApiController
+    {
+            [HttpGet]
+            [Route("api/category")]
+            public HttpResponseMessage All()
+            {
+                var db = new Api2Entities1();
+                var data = db.Categories.ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            [HttpPost]
+            [Route("api/category/create")]
+            public HttpResponseMessage Create(Category obj)
+            {
+                var db = new Api2Entities1();
+                try
+                {
+
+                    db.Categories.Add(obj);
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Created" });
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+                }
+            }
+            [HttpPost]
+            [Route("api/category/update")]
+            public HttpResponseMessage Update(Category upd)
+            {
+                var db = new Api2Entities1();
+                var exobj = db.Categories.Find(upd.Id);
+                db.Entry(exobj).CurrentValues.SetValues(upd);
+                try
+                {
+
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Updated" });
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+                }
+
+            }
+            [HttpPost]
+            [Route("api/category/delete")]
+            public HttpResponseMessage Delete(Category upd)
+            {
+                var db = new Api2Entities1();
+                var exobj = db.Categories.Find(upd.Id);
+                db.Categories.Remove(exobj);
+                try
+                {
+
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Deleted" });
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+                }
+
+            }
+
+        }
+    }
---
+++ api2/Controllers/NewsController.cs
@@ -0,0 +1,78 @@
+﻿using api2.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api2.Controllers
+{
+    public class NewsController : ApiController
+    {
+        [HttpGet]
+        [Route("api/news")]
+        public HttpResponseMessage AllNews()
+        {
+            var dba = new Api2Entities1();
+            var data = dba.News.ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        [HttpPost]
+        [Route("api/news/create")]
+        public HttpResponseMessage CreateNews(News obj)
+        {
+            var db = new Api2Entities1();
+            try
+            {
+
+                db.News.Add(obj);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Created" });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("api/news/update")]
+        public HttpResponseMessage UpdateNews(News upd)
+        {
+            var db = new Api2Entities1();
+            var exobj = db.News.Find(upd.Id);
+            db.Entry(exobj).CurrentValues.SetValues(upd);
+            try
+            {
+
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Updated" });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+        }
+        [HttpPost]
+        [Route("api/news/delete")]
+        public HttpResponseMessage DeleteNews(News upd)
+        {
+            var db = new Api2Entities1();
+            var exobj = db.News.Find(upd.Id);
+            db.News.Remove(exobj);
+            try
+            {
+
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Deleted" });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+        }
+    }
+}
+
